7a22bcfa762c7d6b88e9df6da301ad948bd2064c
HarleyKwyn <kwyn.meagher@gmail.com>
2014-01-27 19:25:03 -0800

    ServeSpec complete

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,6 +4,8 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+var url = require("url");
+var data = [];
 
 var handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
@@ -11,10 +13,27 @@ var handleRequest = function(request, response) {
 
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
+  var statusCode;
+  var parsedURL = url.parse(request.url);
+  console.log(parsedURL.pathname);
+  var validURLs = {
+    "/classes/room1" : true
+  };
 
+  if(validURLs[parsedURL.pathname]){
+      if(request.method === 'GET') {
+        statusCode = 200;
+      } else if (request.method === 'POST'){
+        request.on('data', function (chunk) {
+          data.push(JSON.parse(chunk));
+          console.log("new data: " + JSON.stringify(data, null, 2));
+        });
+      statusCode = 201;
+      }
+    }else{
+      statusCode = 404;
+    }
   console.log("Serving request type " + request.method + " for url " + request.url);
-
-  var statusCode = 200;
 
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
@@ -29,7 +48,9 @@ var handleRequest = function(request, response) {
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+
+   console.log("sending data...: " + JSON.stringify(data, null, 2));
+  response.end(JSON.stringify(data));
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -42,4 +63,8 @@ var defaultCorsHeaders = {
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
+};
+
+module.exports = {
+  handleRequest: handleRequest
 };
